fed0defaf964b90e71bb45defe80ddab9f8a5cc3
keandreacossio36 <keandreacossio36@gmail.com>
2025-03-18 15:02:31 +0000

    Add 13.ts

---
+++ src/13.ts
@@ -0,0 +1,5 @@
+const randomNumber1 = Math.floor(Math.random() * 10) + 1;
+const randomNumber2 = Math.floor(Math.random() * 10) + 1;
+const sum = randomNumber1 + randomNumber2;
+
+console.log(`The sum of ${randomNumber1} and ${randomNumber2} is ${sum}`);
